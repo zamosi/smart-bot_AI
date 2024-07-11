2739a53a04dbae543aa1578ca36919f3ce6b4a3c
zamosi <zamosi@gmail.com>
2024-07-11 23:36:46 +0300

    final step1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import logging
-
+from genai_manager import Gen_ai_Manager
 
 logger = logging.getLogger('my_logger')
 
+def ask_AI(question):
+    q = Gen_ai_Manager(question)
+    return q.generate_response()
 
 if __name__ == '__main__':
-    pass
+    print(ask_AI(''))    
